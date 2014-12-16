7daa6d7fb5029adceb69f3b7d7b95628b60d470c
Khoa Tran <khoatran@berkeley.edu>
2014-12-15 19:51:01 -0800

    Added a few library functions

--- lib.js
+++ lib.js
@@ -18,5 +18,32 @@ module.exports = {
 
   'below': function(x) {
     return function(y) { return y < x; };
-  }
+  },
+
+  'above': function(x) {
+    return function(y) { return y > x; };
+  },
+
+  'equal': function(x) {
+    return function(y) { return y === x; };
+  },
+
+  'sorted': function(x) {
+  	return (function(direction) {
+	    return this.reduce(function(prev, next, i, arr) {
+	      if (direction === undefined)
+	        return (direction = prev <= next ? 1 : -1) || true;
+	      else
+	        return (direction + 1 ?
+	          (arr[i-1] <= next) : 
+	          (arr[i-1] >  next));
+	    }) ? Number(direction) : false;
+	  }).call(this);
+  },
+
+  'all': function(x) {
+    return {'each': function(f) { 
+    	return x.every(f);
+    }};
+  },
 }
